74b6a1bd6070573044c419d2b3d93665137af15b
Livio123123 <99071165+Livio123123@users.noreply.github.com>
2022-03-05 15:44:11 +0700

    Add files via upload

---
+++ tugas8.py
@@ -0,0 +1,16 @@
+
+number = ((1, 2, 3),(4, 5),( 6, 7, 8, 9,10,11)) 
+sum = 0
+for e in number:
+    print(e)
+    for i in e:
+        print(i)
+        sum += i
+print(sum)
+
+
+
+#==data==
+
+#((1, 2, 3),(4, 5),( 6, 7, 8, 9,10,11)) 
+
